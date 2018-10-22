f256ee174fb40982ac05dea68e58fe6aa57c74c9
Mohamed Elshawaf <m_shawaf@outlook.com>
2018-10-23 00:16:35 +0200

    add MVC required folders
    add controllers and views folders

---
+++ src/GroupManagement.Web/Controllers/GroupsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupManagement.Web.Controllers
+{
+    //http://localhost:5000/groups
+    [Route("groups")]
+    public class GroupsController : Controller
+    {
+        [HttpGet,Route("")]
+        public IActionResult Index()
+        {
+            return Content("3Hello dotnettttt!");
+        }
+    }
+}
